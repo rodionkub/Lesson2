81b81a8bc1bbafad5565b5e595851471e5811f58
Ildar Zalyalov <ildarzalyalovmos@gmail.com>
2019-09-26 09:58:00 +0300

    lesson commit

---
+++ Lesson2/ViewControllers/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Lesson2
+//
+//  Created by Ильдар Залялов on 25/09/2019.
+//  Copyright © 2019 Ильдар Залялов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+    }
+}
+
--- Lesson2/ViewControllers/ViewController3.swift
+++ Lesson2/ViewControllers/ViewController3.swift
@@ -1,20 +1,27 @@
 //
-//  ViewController.swift
+//  ViewController3.swift
 //  Lesson2
 //
 //  Created by Ильдар Залялов on 25/09/2019.
 //  Copyright © 2019 Ильдар Залялов. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController3: UIViewController {
+    
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func didPressedBack(_ backButton: UIButton) {
+        
+        navigationController?.popViewController(animated: true)
+    }
 }
-
